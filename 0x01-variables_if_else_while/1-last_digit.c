f084cf1220127551ee711b5f12060e0fbf3907be
mayaka-nyaberi <mayakanyaberi@gmail.com>
2022-12-09 22:36:20 +0300

    0&1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Prints the last digit of a random number
+ *
+ * return - always zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if ((n % 10) > 5)
+{
+	printf("Last digit of %d is %d and is greater than %d\n", n, (n % 10), 5);
+}
+else if ((n % 10) == 0)
+{
+	printf("Last digit of %d is %d and is %d\n", n, (n % 10), 0);
+}
+else if ((n % 10) < 6 && (n % 10) != 0)
+{
+	printf("Last digit of %d is %d and is less than %d and not %d\n", n, (n % 10), 6, 0);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/*
+ *main
+ *
+ *return -always 0
+ */
+int main(){
+  
