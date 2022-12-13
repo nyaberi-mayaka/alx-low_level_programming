bde304501b77dcef7e0f3d35781e51a605ba9d5e
mayaka-nyaberi <mayakanyaberi@gmail.com>
2022-12-13 20:01:44 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "_putchar.h"
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet(void);
+
+	return (0);
+
+}
+
+void print_alphabet(void);
+{
+	char small;
+
+	for (small = 'a'; small <= 'z'; small++)
+	{
+		_putchar('small');
+	}
+
+	_putchar('\n');
+
+}
