0afaff6a6fc0d5e4aca5a9721b1eab6f4dc4a2c9
mayaka-nyaberi <mayakanyaberi@gmail.com>
2022-12-19 19:02:23 +0300

    mandatory

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *start = dest;
 
 	while (src[index])
 	{
-		dest[index] = src[index];
-		index++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
 	*dest = '\0';
 
-	return (dest);
+	return (start);
 }
