390361771d0d31384896405d9dc1032cf7c81a7b
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-01-10 22:05:44 +0300

    malloc, free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 /**
- * free - frees a 2 dimensional grid previously created by malloc function.
+ * free_grid -frees a 2D grid previously created by malloc function.
  * @grid: pointer to a 2D array
  * @height: number of rows.
  *
