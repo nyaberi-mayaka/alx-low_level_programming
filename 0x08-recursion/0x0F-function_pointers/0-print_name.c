fd5d2772970411f28caf07d1353adf7196dd4d3d
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-01-19 19:15:33 +0300

    Function pointers

---
+++ 0x08-recursion/0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: pointer to the string @name.
+ * @f: function pointer taking a @string pointer and returning type void.
+ *
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x08-recursion/0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of
+ * an array.
+ * @array: pointer to the base address of the array of functions.
+ * @size: size of the array.
+ * @action: pointer to the function you need to use.
+ *
+ * Return: void.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
