41e6134e976fe2a4d2b4396ee914b852035d0457
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-01-19 19:29:05 +0300

    Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,13 +17,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
-	{
-		j = cmp(array[i]);
+	if (array && cmp)
+		for (i = 0; i < size; i++)
+		{
+			j = cmp(array[i]);
 
-		if (j != 0)
-			return (i);
-	}
+			if (j != 0)
+				return (i);
+		}
 
 	return (-1);
 }
