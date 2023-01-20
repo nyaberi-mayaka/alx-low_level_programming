d38b979380cd7108469f9458a1c4651c28f1c9b4
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-01-20 19:03:32 +0300

    Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,27 +16,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(strs, n);
 
-	if (n)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			str = va_arg(strs, char *);
+		str = va_arg(strs, char *);
 
-			if (str == NULL)
-				printf("nil");
+		if (str == NULL)
+			printf("nil");
 
-			else
-				printf("%s", str);
+		else
+			printf("%s", str);
 
-			if (i < (n - 1))
-			{
-				if (separator == NULL)
-					continue;
-
-				printf("%s", separator);
-			}
-		}
-	printf("\n");
+		if (i < (n - 1) && separator)
+			printf("%s", separator);
 	}
+
+	printf("\n");
 	va_end(strs);
 }
