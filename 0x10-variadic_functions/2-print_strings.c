65c46ed6e4a59a2357be7769a05e4026b487386f
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-01-20 22:57:46 +0300

    Function pointers

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,33 +1,39 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_strings - prints strings
- * @seperator: printed between the strings
- * @n: number of strings passed to the function
+ * print_strings - prints strings followed by a new line.
+ * @separator: the string to be printed between the strings.
+ * @n: number of strings passed to the function.
+ *
+ * Return: void
  */
 
-void print_strings(const char *seperator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list arg_ptr;
+	size_t i;
+	char *str;
+	va_list strs;
 
-	va_start(arg_ptr, n);
+	va_start(strs, n);
 
 	for (i = 0; i < n; i++)
 	{
-		char *str = va_arg(arg_ptr, char *);
+		str = va_arg(strs, char *);
 
 		if (str == NULL)
 			printf("(nil)");
+
 		else
 			printf("%s", str);
 
-		if (i < (n - 1) && seperator)
-			printf("%s", seperator);
+		if (i < (n - 1))
+		{
+			if (separator == NULL)
+				continue;
+
+			printf("%s", separator);
+		}
 	}
-
 	printf("\n");
-
-	va_end(arg_ptr);
+	va_end(strs);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints everything.
+ * @format: constant pointer to a constant variable.
+ *
+ * Return: void.
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *
