dc48b898da6e951abd84e4c3d1f421f993da4e1b
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-01-21 11:49:11 +0300

    Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,14 +20,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(print_num, int));
 
 
-		if (i < (n - 1))
-		{
-			if (separator == NULL)
-				continue;
-
+		if (i < (n - 1) && separator)
 			printf("%s", separator);
 		}
-	}
 
 	printf("\n");
 	va_end(print_num);
