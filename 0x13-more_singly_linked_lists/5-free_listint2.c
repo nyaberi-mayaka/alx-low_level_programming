752670a18daf0b93d91d9769d445c237cc5e2031
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-02-01 20:51:33 +0300

    More singly linked lists correction to #5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+
+	if (*head == NULL)
+		return;
+
+	temp = *head;
 	while (*head != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		*head = temp->next;
+		free(temp);
+		temp = *head;
 	}
 	*head = NULL;
 }
