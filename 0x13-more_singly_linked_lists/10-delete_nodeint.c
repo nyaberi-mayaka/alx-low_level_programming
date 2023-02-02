559f9286d84d630882c55868cd7ae0efe1dd0696
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-02-02 23:07:48 +0300

    More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,81 +1,39 @@
-#include "lists.h"
+include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node at index @index of a linked list.
- * @head: double pointer to the first node of the linked list.
- * @index: index of the node that should be deleted.
- *
- * Return: 1 if it succeeded, -1 if it failed.
+ * delete_nodeint_at_index - deletetes node at a particular index
+ * @head: head
+ * @index: index
+ * Return: 1 if succeeded, -1 if failed
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *nextnode, *temp;
-	unsigned int i;
+	listint_t *node, *prev_node;
+	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
 
-	if (index > listint_len(*head) - 1)
-		return (-1);
-	i = 0;
-
-	temp =  *head;
-
+	node = *head;
 	if (index == 0)
-		return (pop_listint(head));
-
-	while (i < index - 1)
-		temp = temp->next, i++;
-
-	nextnode = temp->next;
-	temp->next = nextnode->next;
-	free(nextnode);
-
-	if (nextnode == NULL)
-		return (1);
-	else
-		return (-1);
-}
-
-/**
- * listint_len - determines the number of elements in a linked list.
- * @h: pointer to the first node of the list.
- *
- * Return: number of elements in a linked list.
- */
-
-size_t listint_len(const listint_t *h)
-{
-	int i = 0;
-
-	while (h != NULL)
 	{
-		h = h->next;
+		*head = node->next;
+		free(node);
+		return (1);
+	}
+
+	prev_node = *head;
+	while (i < index)
+	{
+		prev_node = node;
+		node = node->next;
+		if (node == NULL)
+			return (-1);
 		i++;
 	}
-	return (i);
-}
 
-/**
- * pop_listint - deletes the head node of a linked list
- * @head: double pointer to the head of the linked list.
- *
- * Return: 1 on success -1 on failure.
- */
-
-int pop_listint(listint_t **head)
-{
-	listint_t *temp;
-
-	if (*head == NULL)
-		return (-1);
-
-	else
-	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
-	}
+	prev_node->next = node->next;
+	free(node);
 	return (1);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,54 +1,44 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a node at a given position.
- * @head:double pointer to the first node of the linked list.
- * @idx: index where the new node should be added.
- * @n: value to be added @idx in the linked list.
- *
- * Return: address of thte new node or null if it failed.
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: list
+ * @idx: index
+ * @n: data/value
+ * Return: address of the new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp;
-	listint_t *newnode = malloc(sizeof(listint_t));
-	unsigned int i;
+	listint_t *new_node, *node;
+	unsigned int i = 1;
 
-	if (newnode == NULL)
+	if (head == NULL)
 		return (NULL);
 
-	if (idx > listint_len(*head) - 1)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	temp = *head;
-	newnode->n = n, newnode->next = NULL;
-
-	i = 0;
-	while (i < idx - 1)
-		temp = temp->next, i++;
-
-	newnode->next = temp->next;
-	temp->next = newnode;
-
-	return (temp->next);
-}
-
-/**
- * listint_len - determines the number of elements in a linked list.
- * @h: pointer to the first node of the list.
- *
- * Return: number of elements in a linked list.
- */
-
-size_t listint_len(const listint_t *h)
-{
-	int i = 0;
-
-	while (h != NULL)
+	new_node->n = n;
+	if (idx == 0)
 	{
-		h = h->next;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	node = *head;
+	while (i < idx)
+	{
+		node = node->next;
+		if (node == NULL)
+			return (NULL);
 		i++;
 	}
-	return (i);
+
+	new_node->next = node->next;
+	node->next = new_node;
+
+	return (new_node);
 }
