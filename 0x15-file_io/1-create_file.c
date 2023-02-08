9cb6b4925de377f29074ae59c2f3a84d6fd72878
mayaka-nyaberi <mayakanyaberi@gmail.com>
2023-02-08 23:56:52 +0300

    C - File I/O in POSIX

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fd_1;
+	int fd;
 	size_t len = 0;
-	ssize_t read_letters, written_letters;
-	char *buff;
+	ssize_t written_letters;
 
 	mode_t mode = S_IRUSR | S_IWUSR;
 
@@ -22,30 +21,20 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, mode);
-	fd_1 = open(text_content, O_RDONLY);
 
-	if (fd < 0 || fd_1 < 0)
+
+	if (fd < 0)
 		return (-1);
+
 	while (text_content[len] != '\0')
 		len++;
 
-	buff = malloc(sizeof(char) * len);
-	if (buff == NULL)
-		return (-1);
-
-	read_letters = read(fd_1, buff, len);
-	if (read_letters == -1)
+	if (text_content != NULL)
 	{
-		free(buff);
-		return (-1);
+		written_letters = write(fd, text_content, len);
+		if (written_letters == -1)
+			return (-1);
 	}
-
-	written_letters = write(fd, buff, read_letters);
-	if (written_letters == -1)
-	{
-		free(buff);
-		return (-1);
-	}
-	close(fd), close(fd_1), free(buff);
+	close(fd);
 	return (1);
 }
