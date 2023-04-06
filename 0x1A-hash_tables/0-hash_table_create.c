30e6301f010aa04cb967f3928a8399bd624060c9
nyaberi <mayakanyaberi@gmail.com>
2023-04-07 02:06:50 +0300

    malloc use

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - a fnction that creates a hash table
+ * hash_table_create - creates a hash table
  * @size: size of the array of type hash_table_t.
  *
- * Return: A pointer to the newly created hash table or
- *         NULL if something went wrong.
+ * Return: Pointer to created hash table or NULL if something went wrong.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -20,11 +19,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!new_table)
 		return (NULL);
+
 	new_table->size = size;
-	new_table->array = calloc(new_table->size, sizeof(hash_node_t *));
+	new_table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (!new_table->array)
+	{
+		free(new_table);
 		return (NULL);
+	}
+
 	for (i = 0; i < new_table->size; i++)
 		new_table->array[i] = NULL;
 
