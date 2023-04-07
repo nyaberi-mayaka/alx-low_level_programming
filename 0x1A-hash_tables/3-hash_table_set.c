ff1fa84a13db368c3e4717ac4d21c921f00aa7e3
nyaberi <mayakanyaberi@gmail.com>
2023-04-07 04:26:51 +0300

    Removed a null pointer read

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *temp = NULL;
 
-	if (ht == NULL || key == NULL)
+	if (!ht || !key)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,8 +14,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp = NULL;
 
-	if (ht->size == 0 || !ht || key == NULL)
+	if (!ht || !key)
 		return (NULL);
+
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[index])
