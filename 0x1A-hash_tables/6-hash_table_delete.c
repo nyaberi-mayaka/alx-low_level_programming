daa5e601f44b7e9882c53357f9e6863dcb0823aa
nyaberi <mayakanyaberi@gmail.com>
2023-04-08 01:15:39 +0300

    6-hash_table_delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	u_long i;
+	hash_node_t *temp = NULL;
+
+	if (ht)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			temp = ht->array[i];
+
+			while (temp)
+			{
+				ht->array[i] = temp->next;
+				free(temp->value), free(temp->key);
+				free(temp);
+				temp = ht->array[i];
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
